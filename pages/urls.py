7475b0cad7ca19ca095a1fdaa6db42c18f1c6438
yeswanthannadata <annadatayeswanth@gmail.com>
2021-09-08 00:08:40 +0530

    added templates views and sample test cases

--- pages/urls.py
+++ pages/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from .views import homePageView
+from .views import HomePageView, AboutPageView
 
 urlpatterns = [
-    path('', homePageView, name="home"),
+    # url for function based view
+    # path('', homePageView, name="home"),
+    path('', HomePageView.as_view(), name="home"),
+    path('about/', AboutPageView.as_view(), name="about"),
 ]
--- pages/views.py
+++ pages/views.py
@@ -1,8 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.generic import TemplateView
 
 # Create your views here.
 
+# function based view example
 
-def homePageView(request):
-    return HttpResponse('Hello World')
+# def homePageView(request):
+#     return HttpResponse('Hello World')
+
+
+class HomePageView(TemplateView):
+    template_name = 'home.html'
+
+
+class AboutPageView(TemplateView):
+    template_name = 'about.html'
