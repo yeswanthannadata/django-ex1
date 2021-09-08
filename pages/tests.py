d28cc032f3772d353231f3925fd7bc6eef3d2e59
yeswanthannadata <annadatayeswanth@gmail.com>
2021-09-08 22:13:59 +0530

    added a test file to check DB create operations

--- pages/tests.py
+++ pages/tests.py
@@ -1,13 +1,25 @@
-from django.test import SimpleTestCase
+from django.test import SimpleTestCase, TestCase
+from django.urls import reverse
 
+from .models import Post
 # Create your tests here.
 
+#simple test case
 
-class SimpleTests(SimpleTestCase):
-    def test_home_page_status_code(self):
-        response = self.client.get('/')
-        self.assertEqual(response.status_code, 200)
+# class SimpleTests(SimpleTestCase):
+#     def test_home_page_status_code(self):
+#         response = self.client.get('/')
+#         self.assertEqual(response.status_code, 200)
 
-    def test_about_page_status_code(self):
-        response = self.client.get('/about/')
-        self.assertEqual(response.status_code, 200)
+#     def test_about_page_status_code(self):
+#         response = self.client.get('/about/')
+#         self.assertEqual(response.status_code, 200)
+
+class PostModelTest(TestCase):
+    def setUp(self):
+        Post.objects.create(test='testing model')
+
+    def test_test_content(self):
+        post = Post.objects.get(id=1)
+        expected_object_name = f'{post.test}'
+        self.assertEqual(expected_object_name, 'testing model')
