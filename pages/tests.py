fc2b6a0c30204d5b227ac3a6cecfd7db95c3d89b
yeswanthannadata <annadatayeswanth@gmail.com>
2021-09-09 22:04:06 +0530

    added a test case for the whole view

--- pages/tests.py
+++ pages/tests.py
@@ -4,7 +4,7 @@ from django.urls import reverse
 from .models import Post
 # Create your tests here.
 
-#simple test case
+# simple test case
 
 # class SimpleTests(SimpleTestCase):
 #     def test_home_page_status_code(self):
@@ -15,6 +15,7 @@ from .models import Post
 #         response = self.client.get('/about/')
 #         self.assertEqual(response.status_code, 200)
 
+
 class PostModelTest(TestCase):
     def setUp(self):
         Post.objects.create(test='testing model')
@@ -23,3 +24,21 @@ class PostModelTest(TestCase):
         post = Post.objects.get(id=1)
         expected_object_name = f'{post.test}'
         self.assertEqual(expected_object_name, 'testing model')
+
+
+class HomePageViewTest(TestCase):
+    def setUp(self):
+        Post.objects.create(test='testing model')
+
+    def test_view_url_existing_at_proper_location(self):
+        resp = self.client.get('/')
+        self.assertEqual(resp.status_code, 200)
+
+    def test_view_url_by_name(self):
+        resp = self.client.get(reverse('home'))
+        self.assertEqual(resp.status_code, 200)
+
+    def test_view_user_correct_template(self):
+        resp = self.client.get(reverse('home'))
+        self.assertEqual(resp.status_code, 200)
+        self.assertTemplateUsed(resp, 'home.html')
