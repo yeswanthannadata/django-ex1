b5967363622634374ef1ee19d4bfcb91dac329a2
yeswanthannadata <annadatayeswanth@gmail.com>
2021-09-09 22:15:10 +0530

    create blogpost model and migrated

--- pages/models.py
+++ pages/models.py
@@ -2,9 +2,18 @@ from django.db import models
 
 # Create your models here.
 
+
 class Post(models.Model):
     test = models.TextField()
 
     def __str__(self):
         return self.test[:50]
 
+
+class BlogPost(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    body = models.TextField()
+
+    def __str__(self):
+        return self.title
